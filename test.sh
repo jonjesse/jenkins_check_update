856641288b2d22bd1d797790f976536defd8f976
root <root@sys1.attlocal.net>
2020-03-28 09:02:02 -0500

    changed test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 _date=$(date +"%m-%d-%y")
-_output=$(docker images | grep $_date)
+_output=$(docker images | grep -i jenkins | grep $_date)
 if [[ $? == 0 ]]; then
   echo "Today $_date jenkins image updated"
 else
