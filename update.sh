642cf1d90ac1619bac565c48fbe67b2bf9e3ecaa
root <root@sys1.attlocal.net>
2020-03-30 13:04:53 -0500

    added digests

--- update.sh
+++ update.sh
@@ -6,11 +6,14 @@ _output=$(docker pull jenkins/jenkins:lts)
 #echo $_output
 _already_exists="Image is up to date"
 _new_image="Downloaded newer image for"
+_digests=""
 
 if [[ $_output =~ $_already_exists ]]; then
   echo "you already have latest image"
 elif [[ $_output =~ $_new_image ]]; then
   echo "new image was downloaded, building"
+  _digests=$(docker image inspect --format='{{.RepoDigests}}' jenkins/jenkins:lts)
+  echo "Digests for new image: $_digests"
   docker build -f Dockerfile -t jenkins/jenkins:lts_docker_$_date .
 else
   echo $_output
